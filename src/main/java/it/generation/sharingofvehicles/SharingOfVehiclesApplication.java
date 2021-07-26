a5273ff5a1a1a1750cfa0b03c70fe1f413d581f9
EOTheDev <edo17maggio@gmail.com>
2021-07-26 11:30:31 +0200

    Initial commit

---
+++ src/main/java/it/generation/sharingofvehicles/SharingOfVehiclesApplication.java
@@ -0,0 +1,13 @@
+package it.generation.sharingofvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingOfVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingOfVehiclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/generation/sharingofvehicles/SharingOfVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.generation.sharingofvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingOfVehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
