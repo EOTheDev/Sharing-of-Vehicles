456ccb40d9fdf296d8c589b2b649ad4263c0a467
EOTheDev <edo17maggio@gmail.com>
2021-07-26 15:11:37 +0200

    update per Giancarlo

---
+++ src/main/java/it/generation/sharingofvehicles/dal/UtenteDAO.java
@@ -0,0 +1,7 @@
+package it.generation.sharingofvehicles.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UtenteDAO extends JpaRepository<Utente, Integer>{
+
+}
