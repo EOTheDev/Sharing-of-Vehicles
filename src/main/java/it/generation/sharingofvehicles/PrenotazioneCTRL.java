372ea9e075600b8e83d3d50a6ae31c7e0a4b8c88
deadman20018 <63054160+deadman20018@users.noreply.github.com>
2021-07-27 15:33:55 +0200

    Add files via upload

---
+++ src/main/java/it/generation/sharingofvehicles/PrenotazioneCTRL.java
@@ -0,0 +1,24 @@
+package it.generation.sharingofvehicles.integration;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.generation.sharingofvehicles.entities.Prenotazione;
+import it.generation.sharingofvehicles.service.PrenotazioneService;
+
+@RestController
+@RequestMapping("/api/prenotazioni")
+public class PrenotazioneCTRL {
+
+	@Autowired
+	private PrenotazioneService ps;
+	
+	@GetMapping("/all")
+	List<Prenotazione> all(){
+		return ps.findAllPrenotazione();
+	}
+}
