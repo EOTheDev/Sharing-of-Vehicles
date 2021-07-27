2c75b4aa6182afc6df33caf7ade5ee702687db3e
Federico Scarlata <84026478+Sca742@users.noreply.github.com>
2021-07-27 11:26:16 +0200

    Update VeicoloDAO.java

--- src/main/java/it/generation/sharingofvehicles/dal/VeicoloDAO.java
+++ src/main/java/it/generation/sharingofvehicles/dal/VeicoloDAO.java
@@ -1,9 +1,20 @@
 package it.generation.sharingofvehicles.dal;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import it.generation.sharingofvehicles.entities.Veicolo;
 
 public interface VeicoloDAO extends JpaRepository<Veicolo, Integer> {
 
+	@Query(value="from Veicolo where tipologia=?1")
+	List<Veicolo> findVeicoloByTipologia(String tipologia);
+	
+	@Query(value="from Veicolo where alimentazione=?1")
+	List<Veicolo> findVeicoloByAlimentazione(String alimentazione);
+	
+	@Query(value="from veicolo where ruote=?1")
+	List<Veicolo> findVeicoloByRuote(int ruote);	
 }
