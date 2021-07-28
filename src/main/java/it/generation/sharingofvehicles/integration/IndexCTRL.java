e25c213219dc9dffd81889e89066c51d5cc1c97c
Federico Scarlata <fedesscarlata@gmail.com>
2021-07-28 22:43:01 +0200

    Update IndexCTRL.java

--- src/main/java/it/generation/sharingofvehicles/integration/IndexCTRL.java
+++ src/main/java/it/generation/sharingofvehicles/integration/IndexCTRL.java
@@ -1,8 +1,10 @@
 package it.generation.sharingofvehicles.integration;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexCTRL {
@@ -13,4 +15,10 @@ public String getIndex() {
 	System.out.println("sono in index");
 	return "home";
 }
+
+@GetMapping("login")
+public ModelAndView login(Model m) {
+	return new ModelAndView("login");
+}
+
 }
