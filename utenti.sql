ea6c772c9336dc22246005f046599e9e8b046c5a
deadman20018 <63054160+deadman20018@users.noreply.github.com>
2021-07-28 09:52:51 +0200

    Update utenti.sql

--- utenti.sql
+++ utenti.sql
@@ -5,3 +5,13 @@ CREATE TABLE `Utenti` (
 	`annoDiNascita` YEAR,
 	`dataIscrizione` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL
 );
+DROP TABLE `Utenti`;
+CREATE TABLE `Utenti` (
+	`userID` INT(50),
+	`nome` VARCHAR(50),
+	`cognome` VARCHAR(50),
+	`email` VARCHAR(50),
+	`firma` VARCHAR(50),
+	`password` VARCHAR(50),
+	`dataIscrizione` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT NULL
+);
