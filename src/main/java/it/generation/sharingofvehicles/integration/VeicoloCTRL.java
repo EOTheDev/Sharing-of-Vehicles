852615ee0990ff3e56ed1e4a1d2f7e10af58ae7d
Federico Scarlata <fedesscarlata@gmail.com>
2021-07-29 12:31:30 +0200

    upd

--- src/main/java/it/generation/sharingofvehicles/integration/VeicoloCTRL.java
+++ src/main/java/it/generation/sharingofvehicles/integration/VeicoloCTRL.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,13 +34,13 @@ public class VeicoloCTRL {
 		return new ModelAndView("singoloVeicolo");
 	}
 	
-//	@GetMapping("/{id}")
-//	public ModelAndView viewAutomobile(Model m, @PathVariable("id") int id) {
-//		
-//		m.addAttribute("veicolo", vs.findVeicoloById(id));
-//		
-//		return new ModelAndView("singoloVeicolo");
-//	}
+	@GetMapping("/{id}")
+	public ModelAndView viewAutomobile(Model m, @PathVariable("id") int id) {
+		
+		m.addAttribute("veicolo", vs.findVeicoloById(id));
+		
+		return new ModelAndView("singoloVeicolo");
+	}
 	
 
 	
