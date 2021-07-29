1c81aaafbe180dcfb58f2be099aae26ad1e1255d
Federico Scarlata <fedesscarlata@gmail.com>
2021-07-29 08:56:11 +0200

    upd

--- src/main/java/it/generation/sharingofvehicles/integration/IndexCTRL.java
+++ src/main/java/it/generation/sharingofvehicles/integration/IndexCTRL.java
@@ -1,16 +1,24 @@
 package it.generation.sharingofvehicles.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import it.generation.sharingofvehicles.service.VeicoloService;
+
 @Controller
 public class IndexCTRL {
 	
+	@Autowired
+	VeicoloService vs;
+	
 @GetMapping("/")
-public String getIndex() {
+public String getIndex(Model m) {
+	
+	m.addAttribute("veicoli",vs.findAllVeicoli());
 	
 	System.out.println("sono in index");
 	return "home";
