d78adae507bd9ff8ba875cedd8ec33b1d7175777
EOTheDev <edo17maggio@gmail.com>
2021-09-07 12:34:40 +0200

    pulizia maven

--- src/main/java/it/generation/sharingofvehicles/SharingOfVehiclesApplication.java
+++ src/main/java/it/generation/sharingofvehicles/SharingOfVehiclesApplication.java
@@ -1,4 +1,4 @@
-package it.generation.sharingofvehicles;
+ package it.generation.sharingofvehicles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- target/classes/static/js/home.js
+++
@@ -1,5 +0,0 @@
-/*aggiorno il link dei veicoli con la data*/   
-    $(".linkVeicolo")
-    .each( function() {
-        $(this).attr("href", $(this).attr("href")+"/data="+dataURL);
-    });
