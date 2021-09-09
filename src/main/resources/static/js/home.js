3711f4a2c5a608e9988459c4e03f7e40f2ad252e
Federico Scarlata <fedesscarlata@gmail.com>
2021-09-09 19:24:55 +0200

    Update home.js

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -33,3 +33,29 @@ $(".editCliente").on("click", function () {
     $("#modificaContainerClienti").attr("style", "display:block;");
 
 });
+
+/*Prenotazione*/
+// rimuovo il popup di modifica clienti
+$("#closeTabPre").on("click", function() {
+    $("#modificaContainerPrenotazioni").attr("style", "display:none;");
+});
+// display di modifica clienti
+$(".editPrenotazione").on("click", function () {
+    console.log(
+
+    );
+
+    // autoinserimento dei valori //
+    //questo è uno span
+    $("#modificaContainerPrenotazioni").find("#questaPrenotazione").text($(this).parent().parent().find("#idPrenotazione").html());
+
+    //questi sono input
+  
+    //questa è una data, attenti è un dito lì. non toccare
+    $("#modificaContainerPrenotazioni").find("#dataPrenotazione").val($(this).parent().find("#day").html());
+    
+    // hidden
+    $("#modificaContainerPrenotazioni").find("#idForm").val($(this).parent().parent().find("#idPrenotazione").html());
+    $("#modificaContainerPrenotazioni").attr("style", "display:block;");
+
+});
